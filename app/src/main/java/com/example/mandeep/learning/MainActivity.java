276e0d72367d2c3fe6e16002894e0687f8805cde
mandeep-vratesh <mandeep.vratesh@gmail.com>
2017-01-23 10:46:23 +0530

    connected buttons to activities

--- app/src/main/java/com/example/mandeep/learning/MainActivity.java
+++ app/src/main/java/com/example/mandeep/learning/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.mandeep.learning;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    private Button operator_learn, operator_practice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.operator_tutorial);
+        setContentView(R.layout.activity_main);
+
+        //relating with ids
+        operator_learn = (Button) findViewById(R.id.operator_learn);
+        operator_practice = (Button) findViewById(R.id.operator_practice);
+
+        operator_learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go_to_learn = new Intent(MainActivity.this,Operator_learn.class);
+                startActivity(go_to_learn);
+            }
+        });
+
+        operator_practice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go_to_learn = new Intent(MainActivity.this,Operator_learn.class);
+                startActivity(go_to_learn);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mandeep/learning/Operator_learn.java
@@ -0,0 +1,16 @@
+package com.example.mandeep.learning;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MANDEEP on 1/23/2017.
+ */
+
+public class Operator_learn extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.operator_tutorial);
+    }
+}
